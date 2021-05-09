3afedc2f2dedb321931ec9b959ac6e145a8e8292
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-05-09 23:55:58 +0100

    initial functionality (#1)
    * initial functionality
    
    * fix test, add badges
    
    * test on more OS's
    
    * exclude 32-bit macOS
    
    * add note
    
    * update workflows

--- src/BenchmarkPlots.jl
+++ src/BenchmarkPlots.jl
@@ -1,5 +1,62 @@
 module BenchmarkPlots
 
-# Write your package code here.
+using UnicodePlots
+using Statistics
+using Printf
+using BenchmarkTools: BenchmarkTools
+
+# Reexport everything *except* `@benchmark`
+for T in setdiff(names(BenchmarkTools), tuple(Symbol("@benchmark")))
+    @eval begin
+        using BenchmarkTools: $T
+        export $T
+    end
+end
+
+# Export our own `@benchmark`
+export @benchmark
+
+
+struct BenchmarkPlot
+    trial::BenchmarkTools.Trial
+end
+
+# borrowed some from `show` implementation for `BenchmarkTools.Trial`
+function Base.show(io::IO, ::MIME"text/plain", bp::BenchmarkPlot)
+    t = bp.trial
+    if length(t) > 0
+        min = minimum(t)
+        max = maximum(t)
+        med = median(t)
+        avg = mean(t)
+        memorystr = string(prettymemory(memory(min)))
+        allocsstr = string(allocs(min))
+        minstr = string(prettytime(time(min)), " (", prettypercent(gcratio(min)), " GC)")
+        maxstr = string(prettytime(time(max)), " (", prettypercent(gcratio(max)), " GC)")
+        medstr = string(prettytime(time(med)), " (", prettypercent(gcratio(med)), " GC)")
+        meanstr = string(prettytime(time(avg)), " (", prettypercent(gcratio(avg)), " GC)")
+    else
+        memorystr = "N/A"
+        allocsstr = "N/A"
+        minstr = "N/A"
+        maxstr = "N/A"
+        medstr = "N/A"
+        meanstr = "N/A"
+    end
+    println(io, "samples: ", length(t), "; evals/sample: ", t.params.evals, "; memory estimate: ", memorystr, "; allocs estimate: ", allocsstr)
+    show(io, histogram(t.times, ylabel="ns", xlabel="Counts", nbins=5))
+    println(io)
+    print(io, "min: ", minstr, "; mean: ", meanstr, "; median: ", medstr, "; max: ", maxstr, ".")
+end
+
+macro benchmark(exprs...)
+    return quote
+        BenchmarkPlot(BenchmarkTools.@benchmark($(exprs...)))
+    end
+end
+
+# We vendor some pretty-printing methods from BenchmarkTools
+# so that we don't have to rely on internals.
+include("vendor.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using BenchmarkPlots
 using Test
 
+
 @testset "BenchmarkPlots.jl" begin
-    # Write your tests here.
+    bp = @benchmark 1+1
+
+    output = sprint(show, MIME"text/plain"(), bp)
+
+    # Don't want to test the exact string since the stats will
+    # fluctuate. So let's just test that it contains the right
+    # number of the right things, and assume they're arranged properly.
+    n_matches = r -> length(collect(eachmatch(r, output)))
+
+    # Top row: timing stats
+    @test n_matches(r"samples:") == 1
+    @test n_matches(r"evals/sample:") == 1
+    @test n_matches(r"memory estimate:") == 1
+    @test n_matches(r"allocs estimate:") == 1
+    # y-axis label + at most four summary stats
+    @test 1 <= n_matches(r"ns") <= 5
+    @test n_matches(r"Counts") == 1
+    # Summary stats
+    @test n_matches(r"min") == n_matches(r"mean") == n_matches(r"median") == n_matches(r"max") == 1
+    @test n_matches(r"% GC") == 4
+    # Corners of the plot
+    @test n_matches(r"┌") == n_matches(r"┐") == n_matches(r"└") == n_matches(r"┘") == 1
 end
